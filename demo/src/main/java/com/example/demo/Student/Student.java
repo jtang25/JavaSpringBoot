da5cd5c62d05dc5be7925cb8c56d3d444c99c65a
jtang25 <jasontang0226@gmail.com>
2023-06-09 03:43:00 -0400

    new update

--- demo/src/main/java/com/example/demo/Student/Student.java
+++ demo/src/main/java/com/example/demo/Student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.Student;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table(name="student")
@@ -22,24 +23,23 @@ public class Student {
     private String email;
     @Column(name = "date_of_birth")
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -59,7 +59,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob,LocalDate.now()).getYears();
     }
 
     public void setId(Long id) {
